b7a5b4bc6c64215d6aaba1632876e048561b4941
Jorge-Leonell-G <jgrimaldog1900@alumno.ipn.mx>
2025-03-06 14:44:23 -0600

    Funcion de division agregada

--- calculadora.c
+++ calculadora.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
+//Declaración de funciones
+float dividir(float a, float b);
+
 int main() {
+
+    float num1, num2, resultado;
     printf("Calculadora inicial\n");
     return 0;
+}
+
+float dividir(float a, float b) {
+    if (b == 0){
+        printf("Error: Division por cero no valida");
+        return 0;
+    }
+    return a / b;
 }
