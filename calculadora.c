be2197c973c1ed6e0f1f9bc2144976343a4eac69
luis03lj <luis.horacio.03@gmail.com>
2025-03-06 16:10:32 -0600

    Función de suma actualizada

--- calculadora.c
+++ calculadora.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
+float suma(float a, float b) {
+    return a + b;
+}
 int main() {
     printf("Calculadora inicial\n");
-    return 0;
+    float n1, n2, sum;
+    printf("Ingrese el primer número: ");
+    scanf("%f", &n1);
+    printf("Ingrese el segundo número: ");
+    scanf("%f", &n2);
+    sum = suma(n1, n2);
+    printf("La suma de %.2f y %.2f es: %.2f\n", n1, n2, sum);
+ return 0;
 }
