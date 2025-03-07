cf4781e8d85fe12198f9a43aac7a9dea99f67f9a
luis03lj <luis.horacio.03@gmail.com>
2025-03-06 18:29:15 -0600

    Función de suma actualizada

--- calculadora.c
+++ calculadora.c
@@ -1,16 +1,73 @@
 #include <stdio.h>
+#include <stdlib.h>  
 
-float suma(float a, float b) {
-    return a + b;
-}
-int main() {
-    printf("Calculadora inicial\n");
-    float n1, n2, sum;
+void suma() {
+    printf("Has seleccionado suma\n");
+    int num1, num2, resultado;
     printf("Ingrese el primer número: ");
-    scanf("%f", &n1);
+    scanf("%d", &num1);
     printf("Ingrese el segundo número: ");
-    scanf("%f", &n2);
-    sum = suma(n1, n2);
-    printf("La suma de %.2f y %.2f es: %.2f\n", n1, n2, sum);
- return 0;
+    scanf("%d", &num2);
+    resultado=num1+num2;
+    printf("La suma de %d y %d es: %d\n", num1, num2, resultado);
+}
+
+void resta() {
+    printf("Has seleccionado resta\n");
+}
+
+void multiplicacion() {
+    printf("Has seleccionado multiplicacion\n");
+}
+
+void division() {
+    printf("Has seleccionado division\n");
+}
+
+void salir(){
+    printf("Saliendo del programa...\n");
+    exit(0);
+}
+
+int main() {
+    int opcion;
+
+    do {
+        system("clear");  // Limpia la pantalla
+
+        printf("==== Menú Principal ====\n");
+        printf("1. Suma\n");
+        printf("2. Resta\n");
+        printf("3. Multiplicacion\n");
+        printf("4. Division\n");
+        printf("5. Salir\n");
+        printf("========================\n");
+        printf("Selecciona una opción: ");
+        scanf("%d", &opcion);
+
+        switch (opcion) {
+            case 1:
+                suma();
+                break;
+            case 2:
+                resta();
+                break;
+            case 3:
+                multiplicacion();
+                break;
+            case 4:
+                division();
+                break;
+            case 5:
+                salir();
+            default:
+                printf("Opción no válida. Intenta de nuevo.\n");
+        }
+        printf("\nPresiona Enter para continuar...");
+        getchar();
+        getchar();  
+
+    } while (opcion != 5);
+
+    return 0;
 }
